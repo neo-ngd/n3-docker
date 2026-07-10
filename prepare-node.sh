9fe91f08af76ee6ae1ea491a8c16c32bb3bd9fbf
superboyiii <573504781@qq.com>
2026-07-10 13:46:30 +0800

    v3.10.1

--- prepare-node.sh
+++ prepare-node.sh
@@ -1,22 +1,33 @@
 echo "Downloading neo node $1"
-wget  https://github.com/neo-project/neo-node/releases/download/$1/neo-cli-linux-x64.zip
-unzip neo-cli-linux-x64.zip
+NEO_DIR="neo-cli-$1"
+ARCHIVE="neo-cli.$1-linux-x64.tar.gz"
+GITHUB_REPO="${GITHUB_REPO:-neo-project/neo-node}"
+wget "https://github.com/${GITHUB_REPO}/releases/download/$1/$ARCHIVE"
+tar -xzf "$ARCHIVE"
 
+if [ ! -d "$NEO_DIR" ]; then
+    echo "Error: expected directory $NEO_DIR after extracting $ARCHIVE" >&2
+    exit 1
+fi
+
+PLUGIN_DIR="./$NEO_DIR"
 
 if [ -z "$2" ]; then
     echo "Downloading plugins $1"
-    wget https://github.com/neo-project/neo-node/releases/download/$1/ApplicationLogs.zip
-    wget https://github.com/neo-project/neo-node/releases/download/$1/RpcServer.zip
-    wget https://github.com/neo-project/neo-node/releases/download/$1/TokensTracker.zip
+    wget "https://github.com/${GITHUB_REPO}/releases/download/$1/ApplicationLogs.zip"
+    wget "https://github.com/${GITHUB_REPO}/releases/download/$1/RpcServer.zip"
+    wget "https://github.com/${GITHUB_REPO}/releases/download/$1/TokensTracker.zip"
 else
     echo "Downloading plugins $2"
-    wget https://github.com/neo-project/neo-node/releases/download/$2/ApplicationLogs.zip
-    wget https://github.com/neo-project/neo-node/releases/download/$2/RpcServer.zip
-    wget https://github.com/neo-project/neo-node/releases/download/$2/TokensTracker.zip
+    wget "https://github.com/${GITHUB_REPO}/releases/download/$2/ApplicationLogs.zip"
+    wget "https://github.com/${GITHUB_REPO}/releases/download/$2/RpcServer.zip"
+    wget "https://github.com/${GITHUB_REPO}/releases/download/$2/TokensTracker.zip"
 fi
 
-unzip -n ApplicationLogs.zip -d ./neo-cli/
-unzip -n RpcServer.zip -d ./neo-cli/
-unzip -n TokensTracker.zip -d ./neo-cli/
+unzip -n ApplicationLogs.zip -d "$PLUGIN_DIR/"
+unzip -n RpcServer.zip -d "$PLUGIN_DIR/"
+unzip -n TokensTracker.zip -d "$PLUGIN_DIR/"
+
+sed -i 's/"BindAddress":[^,]*/"BindAddress": "0.0.0.0"/' "$PLUGIN_DIR/Plugins/RpcServer/RpcServer.json"
 
 echo "Node Ready!"
